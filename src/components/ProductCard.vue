f22e9f549bd0b1dffbfce5464aa7d3cf0726d29e
Devang <devang@dataaro.com>
2026-01-29 00:33:43 +0530

    product list added

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card">
+    <img
+      :src="product.image"
+      :alt="product.title"
+      loading="lazy"
+    />
+
+    <h3>{{ product.title }}</h3>
+    <p>{{ product.category }}</p>
+    <p class="price">${{ product.price }}</p>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  product: {
+    type: Object,
+    required: true
+  }
+})
+</script>
